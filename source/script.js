0b4bffd8054945b4973b63ae982b8e488f22c926
Nastija <nastija.sij@gmail.com>
2025-03-25 21:56:25 +0100

    added Select JavaScript innerHTML

--- source/script.js
+++ source/script.js
@@ -25,6 +25,27 @@ function displayDate() {
   }
 }
 
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+
+  let cityContainerElement = document.querySelector("#city-container");
+  cityContainerElement.innerHTML = `
+      <div class="city-entry">
+       <div class="area" id="los-angeles">
+       <div class="city-info">
+        <p>
+          <span class="city">${cityName}</span>
+          <span id="utc-la">UTC ${cityTime.format("Z")}</span>
+        </p>
+        <p class="date">${cityTime.format("dddd, LL")}</p>
+      </div>
+      <h2 class="time">${cityTime.format("HH:mm:ss")}</h2>   
+      </div>        </div>
+  `;
+}
+
 document.addEventListener("DOMContentLoaded", () => {
   //DOMContentLoaded is an event that fires when the HTML document is fully loaded and parsed, but before images, stylesheets, and other external resources are completely loaded. By default, JavaScript in the <head> runs before the browser finishes loading the HTML. If you try to select an element (document.querySelector) that hasn’t been created yet, JavaScript will return null, causing errors like:
   displayDate();
@@ -32,3 +53,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
 displayDate(); // calls the function
 setInterval(displayDate, 1000); // calls the function every second
+
+let selectCitiesElement = document.querySelector("#select-cities");
+selectCitiesElement.addEventListener("change", updateCity);
